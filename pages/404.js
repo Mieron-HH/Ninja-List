73acfc4b5e5bc218fef27d7f5a34a6c02b672041
NeverLost-Never <76389678+NeverLost-Never@users.noreply.github.com>
2022-03-08 06:10:33 +0400

    NextJs Project - Ninja List

---
+++ pages/404.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+const NotFound = () => {
+	const router = useRouter();
+
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/");
+		}, 3000);
+	}, []);
+
+	return (
+		<>
+			<Head>
+				<title>Ninja List | Page Not Found </title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div className="not-found">
+				<h1>Oooops ..</h1>
+				<h2>That page cannot be found</h2>
+				<p>
+					Go back to the{" "}
+					<Link href="/">
+						<a>Homepage</a>
+					</Link>
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default NotFound;
---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const about = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About </title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+					excepturi eius sequi voluptatem, incidunt non minima distinctio beatae
+					quos qui!
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores
+					excepturi eius sequi voluptatem, incidunt non minima distinctio beatae
+					quos qui!
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default about;
